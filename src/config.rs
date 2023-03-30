935811067a3d9fef0cda8cc9a6b3d98b23527dd6
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-30 15:52:07 +0200

    Changed datatype of config object to hashmap

--- src/config.rs
+++ src/config.rs
@@ -1,35 +1,31 @@
+use hashbrown::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
 use dotenv::dotenv;
 
 const ENV_PREFIX: &str = "PROXRS_";
 
-pub struct Config {
-    pub port: u16,
-    pub addr: [u8; 4],
-}
-
-pub fn config() -> Config {
+pub fn config() -> Arc<Mutex<HashMap<String, String>>> {
     // Load environment variables from .env file
     dotenv().ok().expect("Failed to load .env file");
 
     // Load and parse the port
     let port = std::env::var(format!("{}PORT", ENV_PREFIX))
-        .expect("Failed to load port from environment variables")
-        .parse::<u16>()
-        .expect("Failed to parse port from environment variables");
+        .expect("Failed to load port from environment variables");
 
     // Load and parse the address
     let addr = std::env::var(format!("{}ADDRESS", ENV_PREFIX))
-        .expect("Failed to load address from environment variables")
-        .split('.')
-        .map(|x| {
-            x.parse::<u8>()
-                .expect("Failed to parse address from environment variables")
-        })
-        .collect::<Vec<u8>>();
+        .expect("Failed to load address from environment variables");
+
+    // Load and parse login path
+    let login_path = std::env::var(format!("{}LOGIN_PATH", ENV_PREFIX))
+        .expect("Failed to load login path from environment variables");
 
     // Create and return the config
-    Config {
-        port,
-        addr: [addr[0], addr[1], addr[2], addr[3]],
-    }
+    let mut conf = HashMap::new();
+    conf.insert("port".to_string(), port);
+    conf.insert("addr".to_string(), addr);
+    conf.insert("login_path".to_string(), login_path);
+    Arc::new(Mutex::new(conf))
 }
