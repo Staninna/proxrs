9195fb338fd907d361cac77de61ae121dc6ceb7c
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-31 00:54:19 +0200

    Added basic "Proxy" just auth check

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,9 +1,28 @@
-use crate::session::SessionStore;
-use hyper::{Body, Request, Response};
+use crate::{config::ConfigStore, session::SessionStore, utils::get_session_cookie};
+use hyper::{Body, Request, Response, StatusCode};
 
 pub async fn proxy_handler(
-    _req: Request<Body>,
-    _store: SessionStore,
+    req: Request<Body>,
+    conf: ConfigStore,
+    store: SessionStore,
 ) -> Result<Response<Body>, hyper::Error> {
-    todo!()
+    let session_token = match get_session_cookie(&req, &conf).await {
+        Some(session_token) => session_token,
+        None => {
+            let mut response = Response::new(Body::from("Invalid session"));
+            *response.status_mut() = StatusCode::UNAUTHORIZED;
+
+            return Ok(response);
+        }
+    };
+
+    // Check if the session token is valid
+    if store.get_token(&session_token).await.is_none() {
+        let mut response = Response::new(Body::from("Invalid session"));
+        *response.status_mut() = StatusCode::UNAUTHORIZED;
+
+        return Ok(response);
+    }
+
+    Ok(Response::new(Body::from("Proxy")))
 }
