ec5f2b96e4b4ba132adeeb2bda424ead8a2fc54d
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-01 00:41:55 +0200

    Made activities use json files to store them

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,13 @@
 use crate::config::{ConfigKey::*, ConfigStore};
 use hyper::{Body, Response, StatusCode};
+use serde::Deserialize;
 use tera::{Context, Tera};
+
+#[derive(Deserialize)]
+struct Activity {
+    url: String,
+    description: String,
+}
 
 pub async fn internal_error(
     conf: &ConfigStore,
@@ -9,55 +16,34 @@ pub async fn internal_error(
     // Get the internal error page path from the config
     let internal_error_page_path = conf.get(InternalErrorTemplate).await;
 
-    // Create context for the internal error page
-    let mut context = Context::new();
+    // Get the list of activities
+    // TODO: Make this async
+    // TODO: Make this not unwrap
+    // TODO: Make this configurable in .env
+    // TODO: Make this load on startup and not every time
+    // TODO: Make this use an database
+    let activities: Vec<Activity> = match serde_json::from_str(
+        &std::fs::read_to_string("static/error_activities.json").unwrap(),
+    ) {
+        Ok(activities) => activities,
+        Err(e) => {
+            dbg!(e);
 
-    // Using rand choose and random internet activity
-    let activities = vec![
-        (
-            "https://twitter.com/",
-            "Scroll through endless tweets and procrastinate on real work",
-        ),
-        (
-            "https://youtube.com/watch?v=dQw4w9WgXcQ",
-            "Experience the ultimate 80's nostalgia and get Rickrolled",
-        ),
-        (
-            "https://www.reddit.com/r/funny/",
-            "Get your daily dose of internet humor and memes",
-        ),
-        (
-            "https://www.boredpanda.com/",
-            "Find hilarious and ridiculous stories from around the world",
-        ),
-        (
-            "https://www.netflix.com/",
-            "Waste countless hours watching funny cat videos on Netflix",
-        ),
-        (
-            "https://www.twitch.tv/",
-            "Watch gamers rage quit and fail miserably at video games",
-        ),
-        (
-            "https://www.goodreads.com/",
-            "Discover funny and quirky books to add to your reading list",
-        ),
-        (
-            "https://www.pinterest.com/",
-            "Laugh at Pinterest fails and DIY disasters",
-        ),
-        (
-            "https://www.spotify.com/",
-            "Listen to comedy podcasts and humorous songs",
-        ),
-        (
-            "https://www.etsy.com/",
-            "Shop for ridiculous and hilarious gifts for your friends and family",
-        ),
-    ];
-    let activity = activities[rand::random::<usize>() % activities.len()];
-    context.insert("activity_url", &activity.0);
-    context.insert("activity_description", &activity.1);
+            let mut response = Response::new(Body::from("Internal error while loading activities"));
+            *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            return Ok(response);
+        }
+    };
+
+    // Get a random activity use %
+    let activity = activities
+        .get(rand::random::<usize>() % activities.len())
+        .unwrap();
+
+    // Create the context for the template
+    let mut context = Context::new();
+    context.insert("activity_url", &activity.url);
+    context.insert("activity_description", &activity.description);
 
     // Load the internal error page using the tera template engine
     let mut response = match tera.render(&internal_error_page_path, &context) {
