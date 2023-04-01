940119e4021189467368f46d876ee00df42a392e
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-02 00:44:46 +0200

    Made databse use varchar instead of text

--- src/db.rs
+++ src/db.rs
@@ -31,8 +31,8 @@ impl Db {
         self.conn.lock().await.execute(
             "CREATE TABLE IF NOT EXISTS users (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT NOT NULL UNIQUE,
-                    password TEXT NOT NULL
+                    username VARCHAR(255) NOT NULL UNIQUE,
+                    password VARCHAR(255) NOT NULL
             );",
             [],
         )?;
