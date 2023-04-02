e88cc596bfd24582cf40b117d5500244e2881ebf
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-02 16:11:27 +0200

    Added error type

--- src/config/error.rs
+++ src/config/error.rs
@@ -2,6 +2,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ConfigError {
+    #[error("Missing environment variable: {0}")]
+    MissingEnvVar(String),
+
     #[error("Failed to load `.env` file")]
     DotEnvError,
 
