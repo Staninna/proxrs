84ae1bccbabfbea40ea7460a4de5fe33fc04765a
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-02 23:03:28 +0200

    Renamed Address to IP

--- src/config/options.rs
+++ src/config/options.rs
@@ -3,18 +3,18 @@ use strum_macros::EnumIter;
 // All different config options
 #[derive(Hash, Eq, PartialEq, Debug, Clone, EnumIter)]
 pub enum ConfigOptions {
-    Address,
     DbFile,
     Port,
+    Ip,
 }
 
 // Convert the config options to a string
 impl ToString for ConfigOptions {
     fn to_string(&self) -> String {
         match self {
-            ConfigOptions::Address => "ADDRESS".to_string(),
             ConfigOptions::DbFile => "DB_FILE".to_string(),
             ConfigOptions::Port => "PORT".to_string(),
+            ConfigOptions::Ip => "IP".to_string(),
         }
     }
 }
