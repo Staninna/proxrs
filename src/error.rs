fa0731836986a8d09423301fd9c938b6c966e41e
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-02 23:45:06 +0200

    Added docs to the err macro to explain what it is to outsiders.

--- src/error.rs
+++ src/error.rs
@@ -12,8 +12,30 @@ pub enum Error {
     Database(#[from] crate::db::error::Error),
 }
 
-// Macro to check for errors and print them
-// TODO: Remove line/file/charecter when stable
+/// A macro to check for errors and print them. If an expression evaluates to an error,
+/// it will print the error message, along with the file name, line number, and character
+/// position where the error occurred. The macro will then exit the program with an exit code of 1.
+///
+/// # Examples
+///
+/// ```
+/// use std::fs::File;
+/// use std::io::Read;
+///
+/// fn main() {
+///     // Read the contents of a file
+///     let mut file = err!(File::open("/non/existent/file"));
+///     unreachable!("This code will never be reached");
+/// }
+/// ```
+///
+/// # Notes
+///
+/// This macro uses the `unwrap_or_else` method to handle errors. If the expression being
+/// checked returns an `Ok` value, that value is returned as is. If it returns an `Err`,
+/// the closure passed to `unwrap_or_else` will be executed, which prints the error message
+/// and exits the program. The purpose of this macro is to provide a more expressive way to
+/// handle errors in Rust code, compared to the built-in `?` operator or the `unwrap` method.
 #[macro_export]
 macro_rules! err {
     ($e:expr) => {
