7eef5f9ab7bc6262200b6c696b798b18739e987e
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-10 23:33:08 +0200

    Added new env vars for special route purposes

--- src/conf/options.rs
+++ src/conf/options.rs
@@ -3,6 +3,8 @@ use strum_macros::EnumIter;
 // All different config options
 #[derive(Hash, Eq, PartialEq, Debug, Clone, EnumIter)]
 pub enum ConfigOptions {
+    SpecialRoute,
+    StaticDir,
     DbFile,
     Port,
     Ip,
@@ -12,6 +14,8 @@ pub enum ConfigOptions {
 impl ToString for ConfigOptions {
     fn to_string(&self) -> String {
         match self {
+            ConfigOptions::SpecialRoute => "SPECIAL_ROUTE".to_string(),
+            ConfigOptions::StaticDir => "STATIC_DIR".to_string(),
             ConfigOptions::DbFile => "DB_FILE".to_string(),
             ConfigOptions::Port => "PORT".to_string(),
             ConfigOptions::Ip => "IP".to_string(),
