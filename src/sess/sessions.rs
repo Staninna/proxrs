a41ed684c0fd45733fec2d568b3cd4607f108cb2
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-11 23:00:14 +0200

    Added more docs + added function fo checking tokens before generating

--- src/sess/sessions.rs
+++ src/sess/sessions.rs
@@ -20,19 +20,31 @@ impl Sessions {
         self.store.lock().await
     }
 
-    pub async fn new_session(&mut self, user: String) -> Session {
+    pub async fn new_session(&mut self, user: String) -> Option<Session> {
+        // Check if the user already has a session
+        if let Some(session) = self.get_session_by_user(&user).await {
+            return None;
+        }
+
+        // Create a new session
         let token = Uuid::new_v4().to_string();
         let session = Session::new(user, token.clone());
         self.lock().await.insert(token, session.clone());
-        session
+
+        // Return the session
+        Some(session)
     }
 
     pub async fn get_session_by_user(&self, user: &str) -> Option<Session> {
+        // Enumerate through all the sessions
         for (_, session) in self.lock().await.iter() {
+            // If the session's user is the same as the user, return the session
             if session.user == user {
                 return Some(session.clone());
             }
         }
+
+        // If no session was found, return None
         None
     }
 }
