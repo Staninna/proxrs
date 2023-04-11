8ec9f9be6e067829e1b76ea5c51bc9dc062c07cd
Staninna <26458805+Staninna@users.noreply.github.com>
2023-04-11 15:46:10 +0200

    Added an cookie name

--- src/conf/options.rs
+++ src/conf/options.rs
@@ -4,6 +4,7 @@ use strum_macros::EnumIter;
 #[derive(Hash, Eq, PartialEq, Debug, Clone, EnumIter)]
 pub enum ConfigOptions {
     SpecialRoute,
+    CookieName,
     StaticDir,
     DbFile,
     Port,
@@ -15,6 +16,7 @@ impl ToString for ConfigOptions {
     fn to_string(&self) -> String {
         match self {
             ConfigOptions::SpecialRoute => "SPECIAL_ROUTE".to_string(),
+            ConfigOptions::CookieName => "COOKIE_NAME".to_string(),
             ConfigOptions::StaticDir => "STATIC_DIR".to_string(),
             ConfigOptions::DbFile => "DB_FILE".to_string(),
             ConfigOptions::Port => "PORT".to_string(),
